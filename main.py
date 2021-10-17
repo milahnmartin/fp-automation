090aabec20b51a9a17a83616a6b370fe0429e237
milahnmartin <66036885+milahnmartin@users.noreply.github.com>
2021-10-17 14:21:53 +0200

    added auth for tweepy

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ import requests
 import tweepy
 from dotenv import load_dotenv, find_dotenv
 import os
+import time
 
 load_dotenv(find_dotenv())
 
 
 def auth():
-    pass
+    callback_uri = 'oob'
+    auth = tweepy.OAuthHandler(
+        os.getenv('API_KEY'), os.getenv('API_SECRET_KEY'), callback_uri)
+
+    if(auth):
+        print(True)
+    else:
+        print(False)
 
 
 def main():
@@ -16,4 +24,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    auth()
